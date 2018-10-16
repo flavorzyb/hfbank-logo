a58f9fcd7e58750a6d1f219819dddc5bd03cc4dd
Yanbin Zhu <haker-haker@163.com>
2018-10-16 15:13:16 +0800

    add baidu log

--- spider/hfbank_logo/spiders/config/config.py
+++ spider/hfbank_logo/spiders/config/config.py
@@ -3,7 +3,7 @@
 
 class Config(object):
     url = 'http://image.baidu.com/search/acjson?tn=resultjson_com&ipn=rj&ct=201326592&is=&fp=result&queryWord=%E6%81%92%E4%B8%B0%E9%93%B6%E8%A1%8Clogo&cl=2&lm=-1&ie=utf-8&oe=utf-8&adpicid=&st=-1&z=&ic=0&word=%E6%81%92%E4%B8%B0%E9%93%B6%E8%A1%8Clogo&s=&se=&tab=&width=&height=&face=0&istype=2&qc=&nc=1&fr=&expermode=&rn=30&gsm=5a&1539590783093=&pn='
-    max_page = 1000
+    max_page = 500
 
     @classmethod
     # 百度url
